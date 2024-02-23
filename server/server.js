adb12152656dfa6764320c93180b377dd2751604
Hilario Nengare <hnengare@gmail.com>
2024-02-23 14:54:18 +0300

    nicely done

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { User, Message } = require('./db');
-const { preDefinedUserData } = require('./dummy_data');
+const { preDefinedUserData } = require('./dummyData');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
